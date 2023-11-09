5384f40d7936f2fb1b425a039b1585e62bd3ee41
Gavaskar <gavaskaraeg@gmail.com>
2023-11-09 09:58:56 +0530

    80% of jdbc topics covered

--- src/DBase.java
+++ src/DBase.java
@@ -1,12 +1,17 @@
 import java.sql.*;
 
+import com.mysql.cj.jdbc.CallableStatement;
+
 public class DBase {
 
 	public static void main(String[] args) throws Exception {
-		
-		// insertData();
-		insertUsingPst();
-		readConnection();
+		//System.out.println("hello alians");
+		 // insertData();
+		// insertUsingPst();
+		//update();
+		// readConnection();
+		//deleteData();
+		sp();
 	}
 
 	private static void insertUsingPst()  throws Exception{
@@ -14,8 +19,8 @@ public class DBase {
 		String url = "jdbc:mysql://localhost:3306/jdbcDemo";
 		String userName = "root";
 		String password = "Gava050220#";
-		int id = 3;
-		String name = "varun";
+		int id = 5;
+		String name = "sandhya";
 		int salary = 52000;
 		String Query = "insert into employee values(?,?,?)";
 		Connection con = DriverManager.getConnection(url, userName, password);
@@ -39,6 +44,42 @@ public class DBase {
 		System.out.println("rows added:" + "" + row);
 		con.close();
 	}
+	private static void update() throws Exception {
+		String url = "jdbc:mysql://localhost:3306/jdbcDemo";
+		String userName = "root";
+		String password = "Gava050220#";
+		int id = 5;
+		String Query = "update employee set salary = 60000 where id ="+id;
+		Connection con = DriverManager.getConnection(url, userName, password);
+		Statement st = con.createStatement();
+		int row = st.executeUpdate(Query);
+		System.out.println("rows added:" + "" + row);
+		con.close();
+	}
+	private static void deleteData() throws Exception {
+		String url = "jdbc:mysql://localhost:3306/jdbcDemo";
+		String userName = "root";
+		String password = "Gava050220#";
+		int id = 1;
+		String Query = "delete from employee where id = "+id;
+		Connection con = DriverManager.getConnection(url, userName, password);
+		Statement st = con.createStatement();
+		int row = st.executeUpdate(Query);
+		System.out.println("rows affected:" + "" + row);
+		con.close();
+	}
+	private static void udate() throws Exception {
+		String url = "jdbc:mysql://localhost:3306/jdbcDemo";
+		String userName = "root";
+		String password = "Gava050220#";
+		int id = 1;
+		String Query = "delete from employee where id = "+id;
+		Connection con = DriverManager.getConnection(url, userName, password);
+		Statement st = con.createStatement();
+		int row = st.executeUpdate(Query);
+		System.out.println("rows affected:" + "" + row);
+		con.close();
+	}
 
 	private static void readConnection() throws Exception {
 		String url = "jdbc:mysql://localhost:3306/jdbcDemo";
@@ -58,5 +99,19 @@ public class DBase {
 		con.close();
 
 	}
-
+  public static void sp() throws Exception{
+	  String url = "jdbc:mysql://localhost:3306/jdbcDemo";
+		String userName = "root";
+		String password = "Gava050220#";
+		Connection con = DriverManager.getConnection(url,userName,password);
+		 java.sql.CallableStatement cst =  con.prepareCall("{call getEmployee()}");
+		 ResultSet rs = cst.executeQuery();
+		 while (rs.next()) {
+				System.out.println("***From Db***");
+				System.out.println("id:" + "" + rs.getInt(1));
+				System.out.println("name:" + "" + rs.getString(2));
+				System.out.println("salary:" + "" + rs.getInt(3));
+			}
+		 con.close();
+  }
 }
