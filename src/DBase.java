e630c0158ee2aefa60ebf73e60c7f66fa04cbbf6
Gavaskar <gavaskaraeg@gmail.com>
2023-11-09 11:53:59 +0530

    jdbc completed Mentor:BharathiMam logic first tamil

--- src/DBase.java
+++ src/DBase.java
@@ -11,7 +11,8 @@ public class DBase {
 		//update();
 		// readConnection();
 		//deleteData();
-		sp();
+	//	commit();
+		batchProcessing();
 	}
 
 	private static void insertUsingPst()  throws Exception{
@@ -114,4 +115,94 @@ public class DBase {
 			}
 		 con.close();
   }
+  
+  public static void sp2() throws Exception{
+	  String url = "jdbc:mysql://localhost:3306/jdbcDemo";
+		String userName = "root";
+		String password = "Gava050220#";
+		Connection con = DriverManager.getConnection(url,userName,password);
+		 java.sql.CallableStatement cst =  con.prepareCall("{call getEmpById(?)}");
+		 cst.setInt(1,2);
+		 ResultSet rs = cst.executeQuery();
+		 while (rs.next()) {
+				System.out.println("***From Db***");
+				System.out.println("id:" + "" + rs.getInt(1));
+				System.out.println("name:" + "" + rs.getString(2));
+				System.out.println("salary:" + "" + rs.getInt(3));
+			}
+		 con.close();
+  }
+  
+  public static void spOut() throws Exception{
+	  String url = "jdbc:mysql://localhost:3306/jdbcDemo";
+		String userName = "root";
+		String password = "Gava050220#";
+		Connection con = DriverManager.getConnection(url,userName,password);
+		 java.sql.CallableStatement cst =  con.prepareCall("{call getNameById(?,?)}");
+		 cst.setInt(1, 2);
+		cst.registerOutParameter(2, Types.VARCHAR);
+	   cst.executeUpdate();
+	   System.out.print(cst.getString(2));
+		 con.close();
+  }
+  
+ //commit vs autocommit
+  public static void commit() throws Exception{
+	  String url = "jdbc:mysql://localhost:3306/jdbcDemo";
+		String userName = "root";
+		String password = "Gava050220#";
+		
+		String query1  = "update employee set salary = 300000 where  id =2 ";
+		String query2  = "update employee set salary = 300000 where  id =3";
+
+		Connection con = DriverManager.getConnection(url,userName,password);
+		con.setAutoCommit(false);
+		Statement st = con.createStatement();
+		int row1  = st.executeUpdate(query1);
+		System.out.println(row1+" "+"row affected");
+		
+		int row2  = st.executeUpdate(query2);
+		System.out.println(row2+" "+"row affected");
+		if(row1 >0 && row2 > 0)
+			    con.commit();
+		
+		
+		 con.close();
+  }
+  //batch processing
+  
+  public static void  batchProcessing() throws Exception{
+	  String url = "jdbc:mysql://localhost:3306/jdbcDemo";
+		String userName = "root";
+		String password = "Gava050220#";
+		
+		String query1  = "update employee set salary = 50000 where  id =2 ";
+		String query2  = "update employee set salary = 50000 where  id =3";
+		String query3 = "update employee set salary = 50000  where id = 4";
+
+		Connection con = DriverManager.getConnection(url,userName,password);
+		//for rollback
+		con.setAutoCommit(false);
+		Statement st = con.createStatement();
+		st.addBatch(query1);
+		st.addBatch(query2);
+		st.addBatch(query3);
+	    int [] res = 	st.executeBatch();
+	    
+	    for(int i :res) {
+	    	if(i > 0) {
+	    		continue;
+	    	}
+	    	else {
+	    		con.rollback();
+	    	}
+	    }
+	    con.commit();
+
+		
+		
+		 con.close();
+  }
+  
+  
 }
